90d89f25118f162cf3d8b9492e10a3474fc1d271
Feline Huhn <feline.huhn@gmx.de>
2024-02-08 09:37:44 +0100

    resolve challenge

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "../components/BoxWithStyledComponents.js";
+import styled from "styled-components";
 
 export default function HomePage() {
   return (
-    <div>
+    <BoxContaier>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-    </div>
+      <BoxWithStyledComponents />
+      <BoxWithStyledComponents $isBlack />
+    </BoxContaier>
   );
 }
+
+const BoxContaier = styled.div`
+  display: flex;
+`;
